a8cc581d365905dfbbc76ae106fb95e02803d144
Mohamed Aziz Laifi <152171680+MedAzizL@users.noreply.github.com>
2025-09-14 12:52:09 +0100

    Time: 0 ms (100%), Space: 13.3 MB (43.07%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,8 +3,8 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size(), m = matrix[0].size();
         pair<int,int> l = {0, 0}, r = {n-1, m-1};
-
-        while (true) {
+       
+        while (r>=l) {
             int steps = (r.first - l.first - 1) * m + (m - l.second) + r.second + 1;
             steps /= 2;
 
@@ -28,8 +28,7 @@ public:
                     break;
             }
 
-            if(l.first > r.first || (l.first == r.first && l.second > r.second))
-                break;
+         
         }
 
         return false;
