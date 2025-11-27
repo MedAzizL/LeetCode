c41043def683d17acbe3d1aa98ebc7a738339ecc
Mohamed Aziz Laifi <152171680+MedAzizL@users.noreply.github.com>
2025-11-28 00:44:56 +0100

    Time: 623 ms (54.68%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,3 @@
+select s.user_id , Round(sum(case when c.action='confirmed' then 1 else 0 end ) / count(*) , 2) as confirmation_rate 
+from  Signups as s Left Join confirmations as c on s.user_id=c.user_id
+group by s.user_id;
